b80895270ce867d46901a6a1b5c805ef435fc329
Benjamin Janssens <benji.janssens@gmail.com>
2022-12-20 02:19:52 +0100

    Added support for conversations and removed support for slash commands

--- src/services/slack.service.ts
+++ src/services/slack.service.ts
@@ -8,22 +8,53 @@ const parseSlackMessage = (message: string): string =>
   message.replace(/<@[UW][A-Z0-9]{2,}>/g, '').trim();
 
 export const addEventHandlers = (app: Bolt.App): void => {
-  app.event('app_mention', async ({ event, say }) => {
+  app.event('app_mention', async ({ event, client }) => {
+    let lastReply;
+
+    if (event.thread_ts) {
+      let replies = await client.conversations.replies({
+        channel: event.channel,
+        ts: event.thread_ts,
+        include_all_metadata: true,
+      });
+
+      let messages = replies.messages;
+
+      while (replies.has_more) {
+        replies = await client.conversations.replies({
+          channel: event.channel,
+          ts: event.thread_ts,
+          include_all_metadata: true,
+          cursor: replies.response_metadata.next_cursor,
+        });
+
+        messages = messages.concat(replies.messages);
+      }
+
+      lastReply = messages
+        .reverse()
+        .find((message) => message.metadata?.event_type === 'slackgpt_reply');
+    }
+
     const message = parseSlackMessage(event.text);
 
-    const { response } = await ChatGPTService.sendMessage(message);
+    const { response, conversationId, messageId } =
+      await ChatGPTService.sendMessage(message, {
+        conversationId: lastReply?.metadata.event_payload.conversationId,
+        parentMessageId: lastReply?.metadata.event_payload.messageId,
+      });
 
-    await say({
-      text: response,
+    await client.chat.postMessage({
+      channel: event.channel,
       thread_ts: event.thread_ts || event.ts,
+      text: response,
+      metadata: {
+        event_type: 'slackgpt_reply',
+        event_payload: {
+          conversationId,
+          messageId,
+        },
+      },
     });
-  });
-
-  app.command('/gpt', async ({ command, ack, respond }) => {
-    await ack();
-
-    const { response } = await ChatGPTService.sendMessage(command.text);
-
-    await respond(response);
   });
 };
