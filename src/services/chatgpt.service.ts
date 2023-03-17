cab969128289c184e28f8a18d1fd2b4856dbde03
Benjamin Janssens <benji.janssens@gmail.com>
2023-03-17 23:44:55 +0100

    Replaced node-fetch with isomorphic-fetch

--- src/services/chatgpt.service.ts
+++ src/services/chatgpt.service.ts
@@ -1,18 +1,8 @@
 // Required for Node 16
 import 'node-self';
-import fetch, { RequestInfo, RequestInit, Response } from 'node-fetch';
+import 'isomorphic-fetch';
 
 import { ChatGPTAPI, ChatMessage, SendMessageOptions } from 'chatgpt';
-
-// Required for Node 16
-declare global {
-  function fetch(
-    url: URL | RequestInfo,
-    init?: RequestInit | undefined
-  ): Promise<Response>;
-}
-
-if (!globalThis.fetch) globalThis.fetch = fetch;
 
 export default class ChatGPTService {
   private api: ChatGPTAPI;
